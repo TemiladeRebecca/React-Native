c013028a757ff2d309095f9bf7dffc2335880386
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-10-25 15:39:33 +0100

    Add more padding around list items

--- components/TextInputData.js
+++ components/TextInputData.js
@@ -7,7 +7,7 @@ export default function TextInputData(props) {
     }
     
     return (
-        <View style={{paddingBottom: 10, paddingHorizontal: 16}}>
+        <View style={{paddingVertical: 10, paddingHorizontal: 16}}>
             <Pressable android_ripple={{color: '#dddddd'}} onPress={deleteItem}>
                 <Text style={{color: '#303640', fontSize: 20}}>{props.text}</Text>
             </Pressable>
